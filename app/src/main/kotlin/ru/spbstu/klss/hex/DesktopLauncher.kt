be94477657d2916ace4d2e3c7f8f6bb2c660f351
Sema Buglaev <buglaev.sa@edu.spbstu.ru>
2022-05-17 15:14:58 +0300

    base add

---
+++ app/src/main/kotlin/ru/spbstu/klss/hex/DesktopLauncher.kt
@@ -0,0 +1,17 @@
+package ru.spbstu.klss.hex
+
+import kotlin.jvm.JvmStatic
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import ru.spbstu.klss.hex.model.Hex
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setForegroundFPS(60)
+        config.setTitle("HEX")
+        Lwjgl3Application(Hex(), config)
+    }
+}
---
+++ app/src/main/kotlin/ru/spbstu/klss/hex/model/Hex.kt
@@ -0,0 +1,27 @@
+package ru.spbstu.klss.hex.model
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.ScreenUtils
+
+class Hex : ApplicationAdapter() {
+    var batch: SpriteBatch? = null
+    var img: Texture? = null
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("assets/badlogic.jpg")
+    }
+
+    override fun render() {
+        ScreenUtils.clear(1f, 0f, 0f, 1f)
+        batch!!.begin()
+        batch!!.draw(img, 50f, 50f)
+        batch!!.end()
+    }
+
+    override fun dispose() {
+        batch!!.dispose()
+        img!!.dispose()
+    }
+}
