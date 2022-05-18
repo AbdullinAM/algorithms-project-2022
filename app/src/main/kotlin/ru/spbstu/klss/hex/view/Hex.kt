baa029c4769708413741bf97be4cba56d3bcee1c
Sema Buglaev <buglaev.sa@edu.spbstu.ru>
2022-05-18 20:59:53 +0300

    basic mainmenu

--- app/src/main/kotlin/ru/spbstu/klss/hex/view/Hex.kt
+++ app/src/main/kotlin/ru/spbstu/klss/hex/view/Hex.kt
@@ -7,15 +7,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 class Hex : Game() {
     var batch: SpriteBatch? = null
     var font: BitmapFont? = null
+
     override fun create() {
         batch = SpriteBatch()
         font = BitmapFont() // use libGDX's default Arial font
         setScreen(MainMenuScreen(this))
     }
 
-    override fun render() {
-        super.render() // important!
-    }
 
     override fun dispose() {
         batch!!.dispose()
--- app/src/main/kotlin/ru/spbstu/klss/hex/view/MainMenuScreen.kt
+++ app/src/main/kotlin/ru/spbstu/klss/hex/view/MainMenuScreen.kt
@@ -6,68 +6,97 @@ import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.Button
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
-import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.ScreenUtils
-import javax.swing.event.ChangeEvent
 
 
 class MainMenuScreen(val game: Hex) : Screen {
+    private lateinit var buttom3: ImageButton  // must be rewrite
+    private lateinit var buttom2: ImageButton  // must be rewrite
+    private lateinit var twoPlayerButton: ImageButton
+    private lateinit var startButton: ImageButton
     var camera: OrthographicCamera = OrthographicCamera()
 
-    var stage: Stage? = null
-    var skin: Skin? = null
-    var buttonAtlas: TextureAtlas? = null
-    var table: Table? = null
+    private val stage: Stage = Stage()
+    private val skin: Skin = Skin()
+    private val buttonAtlas: TextureAtlas = TextureAtlas(Gdx.files.internal("assets/packs/start_pack/start.atlas"))
+    private val table: Table = Table()
 
+    private val startButtonStyle: ImageButtonStyle = ImageButtonStyle()
 
-    var button: ImageButton? = null
-    var ImagButtomStyle: ImageButtonStyle? = null
-
-
-    init {
-        this.show()
-    }
 
     override fun show() {
         camera.setToOrtho(false, 1080f, 720f)
-        stage = Stage()
         Gdx.input.inputProcessor = stage
-        skin = Skin()
-        buttonAtlas = TextureAtlas(Gdx.files.internal("assets/packs/start_pack/start.atlas"))
-        skin!!.addRegions(buttonAtlas)
 
-        table = Table()
-        table!!.setFillParent(true)
-        table!!.align(Align.center)
-        table!!.debug(Table.Debug.table)
+        skin.addRegions(buttonAtlas)
 
-        ImagButtomStyle = ImageButtonStyle()
-        ImagButtomStyle!!.up = skin!!.getDrawable("up")
-        ImagButtomStyle!!.down = skin!!.getDrawable("down")
-        ImagButtomStyle!!.checked = skin!!.getDrawable("check")
+        table.setFillParent(true)
+ //       table.debugAll()
 
-        button = ImageButton(ImagButtomStyle)
-        button!!.addListener(object : ChangeListener() {
+        startButtonStyle.up = skin.getDrawable("up")
+        startButtonStyle.down = skin.getDrawable("down")
+        startButtonStyle.checked = skin.getDrawable("check")
+
+        startButton = ImageButton(startButtonStyle)
+        startButton.addListener(object : ChangeListener() {
             override fun changed(event: ChangeEvent?, actor: Actor?) {
-                TODO("Not yet implemented")
+                twoPlayerButton.isVisible = !twoPlayerButton.isVisible
+                buttom2.isVisible = !buttom2.isVisible
+                buttom3.isVisible = !buttom3.isVisible
+                changePadOf(startButton)
+                table.invalidate()
             }
         }
         )
 
-        table!!.add(button)//.padLeft(1080f)//.padBottom(900f)
-//		table!!.row()
-//		table!!.add(ImageButton(nimb))//.padLeft(1080f).padBottom(900f)
-        stage!!.addActor(table)
+        twoPlayerButton = ImageButton(startButtonStyle)// must be override !!!!
+        twoPlayerButton.addListener(object : ChangeListener() {
+                override fun changed(event: ChangeEvent?, actor: Actor?) {
+                    game.screen = GameScreen()
+                }
+            }
+            )
+
+        buttom2 = ImageButton(startButtonStyle)// must be override !!!!
+        buttom3 = ImageButton(startButtonStyle)// must be override !!!!
+        twoPlayerButton.isVisible = false
+        buttom2.isVisible = false
+        buttom3.isVisible = false
+
+        table.bottom()
+        table.add(twoPlayerButton)
+        table.row()
+        table.add(buttom2)
+        table.row()
+        table.add(buttom3)
+        table.row()
+        table.add(startButton).padBottom(stage.height * 0.7f)
+
+        stage.addActor(table)
+    }
+
+    private fun changePadOf(button: Button) {
+        var cell = table.getCell(button)
+        if (twoPlayerButton.isVisible) {
+            cell.padBottom(0f)
+            cell.padTop(table.height * 0.7f - 3 * button.height)
+
+        } else {
+            cell.padBottom(table.height * 0.7f)
+            cell.padTop(0f)
+        }
+
     }
 
     override fun render(delta: Float) {
         ScreenUtils.clear(0f, 0f, 0.2f, 1f)
-        stage!!.draw()
+        stage.draw()
 
     }
 
