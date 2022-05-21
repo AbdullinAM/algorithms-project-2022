cb01dc4e67bb0e5de2063f82ccd0d4b6bcaf52d9
Sema Buglaev <buglaev.sa@edu.spbstu.ru>
2022-05-21 19:37:38 +0300

    SemaSolver

--- app/src/main/kotlin/ru/spbstu/klss/hex/solver/SemaSolver.kt
+++ app/src/main/kotlin/ru/spbstu/klss/hex/solver/SemaSolver.kt
@@ -2,301 +2,240 @@ package ru.spbstu.klss.hex.solver
 
 import ru.spbstu.klss.hex.model.Cell
 import ru.spbstu.klss.hex.model.Color
-import kotlin.math.abs
+import ru.spbstu.klss.hex.model.Model
 import kotlin.math.max
 import kotlin.math.min
 
-class SemaSolver(color: Color) : Solver{
+class SemaSolver(color: Color) : Solver {
 
     val selfColor = color
     val enemyColor = if (selfColor == Color.RED) Color.BLUE else Color.RED
-    val limitDepth = 3
+    val limitDepth = 1
 
-    val bridgeScore = 2
-    val savedBridgeScore = 4
-    val sizeMultiplier = 10
-    val cellCountMultiplier = 1
-    val attackedBridgeScore = -6
-    val blockedEnemyBridgeScore = 4
+    //koef
+    private val closeConnectMultiplayer = 5
+    private val emptyConnectMultiplayer = 10
 
-    override fun action(board: MutableList<Cell>): Pair<Int, Int> { // returns X and Y
+    private lateinit var finalCell: Cell
+
+
+    override fun action(model: Model): Pair<Int, Int> { // returns X and Y
+        val board = model.board
         if (getCells(board, selfColor).size == 0) {
-            if (board[5 * 11 + 5].color == Color.GRAY)
-                return Pair(5, 5)
+            return if (board[5 * 11 + 5].color == Color.GRAY)
+                Pair(5, 5)
             else
-                return Pair(4, 5)
+                Pair(6, 5)
         }
 
         var maxScore = Int.MIN_VALUE
-        var result_x = -1
-        var result_y = -1
+        var resultX = -1
+        var resultY = -1
         for (element in getCells(board, Color.GRAY)) {
             element.color = selfColor
-            val stepScore = alphaBeta(board, 1, Int.MIN_VALUE, Int.MAX_VALUE)
+            val stepScore = alphaBeta(board, 1, Int.MIN_VALUE, Int.MAX_VALUE, false)
             if (stepScore > maxScore) {
                 maxScore = stepScore
-                result_x = element.x
-                result_y = element.y
+                resultX = element.x
+                resultY = element.y
             }
             element.reset()
         }
-        return Pair(result_x, result_y)
+        return Pair(resultX, resultY)
     }
 
-    fun minimax(board: MutableList<Cell>, depth: Int): Int {
-        val scoreList = mutableListOf<Int>()
-        var result = if (depth % 2 == 1) Int.MAX_VALUE else Int.MIN_VALUE
-        if (depth == limitDepth) return countScore(board)
+
+    fun alphaBeta(board: MutableList<Cell>, depth: Int, alpha: Int, beta: Int, ourTurn: Boolean): Int {
+
+        if (depth == limitDepth) return evaluaionOfPosition(board)
+        var bestEval = 0
+        var curAlfa = alpha
+        var curBeta = beta
+
         for (element in getCells(board, Color.GRAY)) {
-            element.color = if (depth % 2 == 1) enemyColor else selfColor
-            val score = minimax(board, depth + 1)
-            scoreList.add(score)
-            element.reset()
-            if (depth % 2 == 1) {
-                result = min(result, score)
+            if (ourTurn) {
+
+                element.color = selfColor
+                bestEval = Int.MIN_VALUE
+                val eval = alphaBeta(board, depth + 1, curAlfa, curBeta, false)
+                element.reset()
+                bestEval = max(bestEval, eval)
+                curAlfa = max(curAlfa, eval)
 
             } else {
-                result = max(result, score)
-            }
-        }
-        //return if (depth % 2 == 1) scoreList.minOrNull() ?: -1000 else scoreList.maxOrNull() ?: 1000
-        return result
-    }
 
-    fun alphaBeta(board: MutableList<Cell>, depth: Int, alpha: Int, beta: Int): Int {
-        var result = if (depth % 2 == 1) Int.MAX_VALUE else Int.MIN_VALUE
-        var beta = beta
-        var alpha = alpha
-        if (depth == limitDepth) return countScore(board)
-        for (element in getCells(board, Color.GRAY)) {
-            element.color = if (depth % 2 == 1) enemyColor else selfColor
-            val score = alphaBeta(board, depth + 1, alpha, beta)
-            element.reset()
-            if (depth % 2 == 1) {
-                result = min(result, score)
-                beta = min(beta, score)
+                element.color = enemyColor
+                bestEval = Int.MAX_VALUE
+                val eval = alphaBeta(board, depth + 1, curAlfa, curBeta, true)
+                element.reset()
+                bestEval = min(bestEval, eval)
+                curBeta = min(curBeta, eval)
 
-            } else {
-                result = max(result, score)
-                alpha = max(alpha, score)
             }
-            if (beta <= alpha) {
+            if (curBeta <= curAlfa) {
                 break
             }
         }
-        return result
+        return bestEval
     }
 
-    fun countScore(board: MutableList<Cell>): Int {
+    fun evaluaionOfPosition(board: MutableList<Cell>): Int {
         var score = 0
-        score += (countBridges(board, selfColor) - countBridges(board, enemyColor)) * bridgeScore
-        score += countMaxLength(board, selfColor) - countMaxLength(board, enemyColor) * sizeMultiplier
-        score += (countAttackedBridges(board, selfColor) - countAttackedBridges(board, enemyColor)) * attackedBridgeScore
-        score += (countSavedBridges(board, selfColor) - countSavedBridges(board, enemyColor)) * savedBridgeScore
-        score += (countEnemyBlockedBridges(board, selfColor) - countEnemyBlockedBridges(board, enemyColor)) * blockedEnemyBridgeScore
-        score += (getCells(board, selfColor).size - getCells(board, enemyColor).size) * cellCountMultiplier
-        if (countMaxLength(board, selfColor) == 11) score += 1000
-        else if (countMaxLength(board, enemyColor) == 11) score -= 1000
+
+        score += closeConnectionCount(board) * closeConnectMultiplayer
+        score += emptyConnectionCount(board) * emptyConnectMultiplayer
+
+
         return score
     }
 
-    fun countBridges(board: MutableList<Cell>, color: Color): Int {
-        var bridgeCount = 0
-        val currentCellList = getCells(board, color)
-
-        for (i in 0 until currentCellList.size) {
-            for (j in i + 1 until currentCellList.size) {
-                val cell1 = currentCellList[i]
-                val cell2 = currentCellList[j]
-                if (isBridge(cell1, cell2) and !isBlockedBridge(cell1, cell2, board)) {
-                    bridgeCount++
-                }
-            }
-        }
-        return bridgeCount
-    }
-
-
-    fun countMaxLength(board: MutableList<Cell>, currentColor: Color): Int {
-        val currentCellList = getCells(board, currentColor)
-        var minCoord: Int
-        var maxCoord: Int
-        var maxLenght = 0
-        val visited = mutableSetOf<Cell>()
-
-        for (cell in currentCellList) {
-            if (cell in visited) continue
-            val stack = ArrayDeque<Pair<Cell, MutableIterator<Cell>>>()
-            var currentCell = cell
-            var currentIterator = currentCell.neighbours.iterator()
-            visited.add(currentCell)
-            stack.add(Pair(currentCell, currentIterator))
-
-            minCoord = Int.MAX_VALUE
-            maxCoord = Int.MIN_VALUE
-
-            while (stack.isNotEmpty()) {
-                currentCell = stack.last().first
-                currentIterator = stack.last().second
-                while (currentIterator.hasNext()) {
-                    if (currentColor == Color.BLUE) {
-                        minCoord = min(minCoord, currentCell.y)
-                        maxCoord = max(maxCoord, currentCell.y)
-                    } else if (currentColor == Color.RED) {
-                        minCoord = min(minCoord, currentCell.x)
-                        maxCoord = max(maxCoord, currentCell.x)
-                    }
-
-                    val nextCell = currentIterator.next()
-                    if (nextCell.color != currentColor || nextCell in visited) continue
-
-                    visited.add(currentCell)
-                    currentCell = nextCell
-                    currentIterator = nextCell.neighbours.iterator()
-                    stack.add(Pair(currentCell, currentIterator))
-                }
-                stack.removeLast()
-            }
-
-            maxLenght = max(maxLenght, maxCoord - minCoord + 1)
-        }
-        return maxLenght
-    }
-
-    fun countSavedBridges(board: MutableList<Cell>, currentColor: Color): Int {
-        var bridgeCount = 0
-        val currentCellList = getCells(board, currentColor)
-
-        for (i in 0 until currentCellList.size) {
-            for (j in i + 1 until currentCellList.size) {
-                val cell1 = currentCellList[i]
-                val cell2 = currentCellList[j]
-                if (isSavedBridge(cell1, cell2, board)) {
-                    bridgeCount++
-                }
-            }
-        }
-        return bridgeCount
-    }
-
-    fun countAttackedBridges(board: MutableList<Cell>, currentColor: Color): Int {
-        var bridgeCount = 0
-        val currentCellList = getCells(board, currentColor)
-
-        for (i in 0 until currentCellList.size) {
-            for (j in i + 1 until currentCellList.size) {
-                val cell1 = currentCellList[i]
-                val cell2 = currentCellList[j]
-                if (isAttackedBridge(cell1, cell2, board)) {
-                    bridgeCount++
-                }
-            }
-        }
-        return bridgeCount
-    }
-
-    fun countEnemyBlockedBridges(board: MutableList<Cell>, currentColor: Color): Int {
-        var bridgeCount = 0
-        val oppositeColor = if (currentColor == Color.RED) Color.BLUE else Color.RED
-        val currentCellList = getCells(board, oppositeColor)
-
-        for (i in 0 until currentCellList.size) {
-            for (j in i + 1 until currentCellList.size) {
-                val cell1 = currentCellList[i]
-                val cell2 = currentCellList[j]
-                if (isBlockedBridge(cell1, cell2, board)) {
-                    bridgeCount++
-                }
-            }
-        }
-        return bridgeCount
-    }
-
-    fun isBridge(cell1: Cell, cell2: Cell): Boolean {
-        if (cell1.color != cell2.color || Color.GRAY in listOf(cell1.color, cell2.color)) return false
-        val deltaX = cell1.x - cell2.x
-        val deltaY = cell1.y - cell2.y
-        val resultMultiplication = deltaX * deltaY
-        if (resultMultiplication == 2 || resultMultiplication == -1) return true
+    fun isWinnigWay(board: MutableList<Cell>,color: Color):Boolean {
+        if(isWinner(color,board)) return true
         return false
     }
 
-    fun isBlockedBridge(cell1: Cell, cell2: Cell, board: MutableList<Cell>): Boolean {
-        if (!isBridge(cell1, cell2)) return false
-        val deltaX = cell1.x - cell2.x
-        val deltaY = cell1.y - cell2.y
-        val bridgeNeibhours: Pair<Cell, Cell>
-        val oppositeColor = if (cell1.color == Color.RED) Color.BLUE else Color.RED
-
-        if (abs(deltaX) == 2) bridgeNeibhours = Pair(
-            board[countIndex((cell1.x + cell2.x) / 2, cell1.y)],
-            board[countIndex((cell1.x + cell2.x) / 2, cell2.y)]
-        ) else if (abs(deltaY) == 2) bridgeNeibhours = Pair(
-            board[countIndex(cell1.x, (cell1.y + cell2.y) / 2)],
-            board[countIndex(cell2.x, (cell1.y + cell2.y) / 2)]
-        ) else bridgeNeibhours = Pair(
-            board[countIndex(cell1.x, cell2.y)],
-            board[countIndex(cell2.x, cell1.y)]
-        )
-
-        if (
-            bridgeNeibhours.first.color == oppositeColor &&
-            bridgeNeibhours.second.color == bridgeNeibhours.first.color
-        ) return true
+    fun isWinner(color: Color, board: MutableList<Cell>): Boolean {
+        if (color == Color.RED) {
+            for (y in 0..10) {
+                val cell = board[11 * y]
+                if (hasPath(cell, color)) return true
+            }
+        } else if (color == Color.BLUE) {
+            for (x in 0..10) {
+                val cell = board[x]
+                if (hasPath(cell, color)) return true
+            }
+        }
         return false
-
-
     }
 
-    fun isAttackedBridge(cell1: Cell, cell2: Cell, board: MutableList<Cell>): Boolean {
-        if (!isBridge(cell1, cell2)) return false
-        val deltaX = cell1.x - cell2.x
-        val deltaY = cell1.y - cell2.y
-        val bridgeNeibhours: Pair<Cell, Cell>
-        val oppositeColor = if (cell1.color == Color.RED) Color.BLUE else Color.RED
+    fun hasPath(cell: Cell, color: Color): Boolean {
+        if (cell.color == Color.GRAY) return false
+        val stack = ArrayDeque<Pair<Cell, MutableIterator<Cell>>>()
+        var currentCell = cell
+        var currentIterator = currentCell.neighbours.iterator()
+        val visited = mutableSetOf(currentCell)
+        stack.add(Pair(currentCell, currentIterator))
 
-        if (abs(deltaX) == 2) bridgeNeibhours = Pair(
-            board[countIndex((cell1.x + cell2.x) / 2, cell1.y)],
-            board[countIndex((cell1.x + cell2.x) / 2, cell2.y)]
-        ) else if (abs(deltaY) == 2) bridgeNeibhours = Pair(
-            board[countIndex(cell1.x, (cell1.y + cell2.y) / 2)],
-            board[countIndex(cell2.x, (cell1.y + cell2.y) / 2)]
-        ) else bridgeNeibhours = Pair(
-            board[countIndex(cell1.x, cell2.y)],
-            board[countIndex(cell2.x, cell1.y)]
-        )
+        while (stack.isNotEmpty()) {
+            currentCell = stack.last().first
+            currentIterator = stack.last().second
+            while (currentIterator.hasNext()) {
+                val nextCell = currentIterator.next()
+                if (nextCell.color != color || nextCell in visited) continue
 
-        if (
-            Color.GRAY in listOf(bridgeNeibhours.first.color, bridgeNeibhours.second.color) &&
-            oppositeColor in listOf(bridgeNeibhours.first.color, bridgeNeibhours.second.color)
-        ) return true
+                if (color == nextCell.color && nextCell.color == Color.BLUE && nextCell.y == 10) return true
+                if (color == nextCell.color && nextCell.color == Color.RED && nextCell.x == 10) return true
+
+                visited.add(currentCell)
+                currentCell = nextCell
+                currentIterator = nextCell.neighbours.iterator()
+                stack.add(Pair(currentCell, currentIterator))
+            }
+            stack.removeLast()
+        }
         return false
-
-
     }
 
-    fun isSavedBridge(cell1: Cell, cell2: Cell, board: MutableList<Cell>): Boolean {
-        if (!isBridge(cell1, cell2)) return false
-        val deltaX = cell1.x - cell2.x
-        val deltaY = cell1.y - cell2.y
-        val bridgeNeibhours: Pair<Cell, Cell>
+    fun longestWay(board: MutableList<Cell>, color: Color): MutableList<Cell> {
+        val allColorCells = getCells(board, color)
+        var longestWay = mutableListOf<Cell>()
+        for (cell in allColorCells) {
+            val mapOfWays = findWays(cell)
+            val curWay = mutableListOf<Cell>()
 
-        if (abs(deltaX) == 2) bridgeNeibhours = Pair(
-            board[countIndex((cell1.x + cell2.x) / 2, cell1.y)],
-            board[countIndex((cell1.x + cell2.x) / 2, cell2.y)]
-        ) else if (abs(deltaY) == 2) bridgeNeibhours = Pair(
-            board[countIndex(cell1.x, (cell1.y + cell2.y) / 2)],
-            board[countIndex(cell2.x, (cell1.y + cell2.y) / 2)]
-        ) else bridgeNeibhours = Pair(
-            board[countIndex(cell1.x, cell2.y)],
-            board[countIndex(cell2.x, cell1.y)]
-        )
+            var curCell = finalCell
+            while (curCell != cell) {
+                curWay.add(curCell)
+                curCell = mapOfWays[curCell]!!
+            }
+            if (curWay.size > longestWay.size)
+                longestWay = curWay
+        }
+        return longestWay
+    }
 
-        if (
-            (bridgeNeibhours.first.color == Color.RED) and
-            (bridgeNeibhours.second.color != bridgeNeibhours.first.color)
-        ) return true
-        return false
+    fun findWays(cell: Cell): MutableMap<Cell, Cell> {
+        val mapCameFrom = mutableMapOf<Cell, Cell>()
+        val cellColor = cell.color
+        if (cellColor == Color.GRAY) return mapCameFrom
+
+        val stack = ArrayDeque<Cell>()
+        stack.add(cell)
+
+        while (stack.isNotEmpty()) {
+            val currentCell = stack.removeLast()
+            for (neighbour in currentCell.neighbours) {
+                if (neighbour.color == cellColor && neighbour !in mapCameFrom.values) {
+                    stack.add(neighbour)
+                    mapCameFrom[neighbour] = currentCell
+                    finalCell = neighbour
+                }
+                if (neighbour.color == Color.GRAY) {
+                    for (doubleNeighbour in neighbour.neighbours)
+                        if (hasEmptyConnect(currentCell, doubleNeighbour)) {
+                            mapCameFrom[neighbour] = currentCell
+                            mapCameFrom[doubleNeighbour] = neighbour
+                            finalCell = doubleNeighbour
+                        }
+                }
+            }
+        }
+        return mapCameFrom
+    }
+
+    fun closeConnectionCount(board: MutableList<Cell>): Int {
+        var result = 0
+        for (cell in board) {
+            if (cell.color == selfColor) result += cellCloseConnect(cell).size
+            if (cell.color == enemyColor) result -= cellCloseConnect(cell).size
+        }
+        return result
+    }
+
+    fun cellCloseConnect(cell: Cell): HashSet<Cell> {
+        val result = hashSetOf<Cell>()
+        for (neighbour in cell.neighbours) {
+            if (cell.color == neighbour.color) result.add(neighbour)
+        }
+        return result
+    }
+
+    fun emptyConnectionCount(board: MutableList<Cell>): Int {
+        var result = 0
+        for (y in 0..10) {
+            for (x in 0..10) {
+                val currentCell = board[11 * y + x]
+                if (currentCell.color == Color.GRAY) continue
+                var connectionsCount = 0
+
+                if (y != 0 && x != 10) {
+                    val checkedCell = board[11 * (y - 1) + (x + 1)]
+                    if (hasEmptyConnect(currentCell, checkedCell)) connectionsCount++
+                }
+                if (y != 10 && x < 9) {
+                    val checkedCell = board[11 * (y + 1) + (x + 2)]
+                    if (hasEmptyConnect(currentCell, checkedCell)) connectionsCount++
+                }
+                if (y < 9 && x != 10) {
+                    val checkedCell = board[11 * (y + 2) + (x + 1)]
+                    if (hasEmptyConnect(currentCell, checkedCell)) connectionsCount++
+                }
+
+                if (currentCell.color == enemyColor) connectionsCount *= -1
+                result += connectionsCount
+            }
+        }
+        return result
+    }
+
+    fun hasEmptyConnect(cell1: Cell, cell2: Cell): Boolean {
+        if (cell1.color != cell2.color) return false
+        var ways = 0
+        for (neighbour in cell1.neighbours) {
+            if (neighbour.color == Color.GRAY && cell2 in neighbour.neighbours) ways++
+        }
+        return ways == 2
     }
 
     fun countIndex(x: Int, y: Int) = y * 11 + x
